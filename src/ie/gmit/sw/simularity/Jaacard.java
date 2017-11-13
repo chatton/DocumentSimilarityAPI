4ae5795e68fae2582da4b61bbbdee863ce68183f
chatton <cianhatton@gmail.com>
2017-11-13 16:17:42 +0000

    added simularity index and empty Jaacard implementation

---
+++ src/ie/gmit/sw/simularity/Jaacard.java
@@ -0,0 +1,10 @@
+package ie.gmit.sw.simularity;
+
+
+public class Jaacard implements SimularityIndex{
+
+    @Override
+    public double computeIndex() {
+        return 0;
+    }
+}
---
+++ src/ie/gmit/sw/simularity/SimularityIndex.java
@@ -0,0 +1,5 @@
+package ie.gmit.sw.simularity;
+
+public interface SimularityIndex {
+    double computeIndex();
+}
